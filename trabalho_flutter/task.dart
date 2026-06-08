a9d2a8e0328787684f4d791008a1ce42ff1b8d13
joaonrique <158644680+joaonrique@users.noreply.github.com>
2026-06-08 17:59:31 -0300

    Update task.dart

--- trabalho_flutter/task.dart
+++ trabalho_flutter/task.dart
@@ -1,1 +1,12 @@
 
+class Task {
+  int? id;
+  String title;
+
+  Task({this.id, required this.title});
+
+  Map<String, dynamic> toMap() => {'id': id, 'title': title};
+
+  factory Task.fromMap(Map<String, dynamic> map) => 
+      Task(id: map['id'], title: map['title']);
+}
